d3a2768b821f1520c3828cebc743a2af73edb7bc
liu <liu@example.com>
2019-09-23 16:07:51 +0800

    jianshu

---
+++ build/precache-manifest.96ecd02e29daf331df12e463e81ec417.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6707ff2434f80ad72431ef5809460f3f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "6f2aca88f0a8bacbad74",
+    "url": "./static/js/2.8b405070.chunk.js"
+  },
+  {
+    "revision": "8ed43905fb6b70799926",
+    "url": "./static/js/3.7bf95dfb.chunk.js"
+  },
+  {
+    "revision": "e65dd1dd0ed75d68997b",
+    "url": "./static/js/main.79b308c9.chunk.js"
+  },
+  {
+    "revision": "6ec7aafa8eb28809ff04",
+    "url": "./static/js/runtime~main.0f3f35b4.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.84e7eb78be31dec4cafc52fb18bae363.js"
+  "./precache-manifest.96ecd02e29daf331df12e463e81ec417.js"
 );
 
 self.addEventListener('message', (event) => {
